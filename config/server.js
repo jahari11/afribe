e2f7eef617e8b89451548ea984034acdbfa40685
Jahari <jahari.crawford.young@gmail.com>
2024-03-24 09:52:46 -0400

    server updates

--- config/server.js
+++ config/server.js
@@ -1,8 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  url:env('https://afri-new-e8b16c3eb9ad.herokuapp.com'),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['u5nc0k8SAJz5PawBHbbnuQ==,/tw6SSrddb3WwqxWB9tdhg==,q815jXzq8W5QgqxV5gzxGw==,URYGYEgjr3chdo1Jv5yijQ==']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
